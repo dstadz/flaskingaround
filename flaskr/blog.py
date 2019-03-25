9f0282338168371d7f94ae49acb6967809d86d5e
Drazen <danstad2012@gmail.com>
2019-03-25 13:17:42 -0700

    layout template

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -2,17 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+    'author': "alice",
+    'title': "post 1",
+    'content': 'first post',
+    'date': '3/25/19'
+    },  {
+    'author': "bob",
+    'title': "post 2",
+    'content': 'second post',
+    'date': '3/26/19'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 
 @app.route('/about')
 def about():
-    return '<h1>about page</h1>'
+    return render_template('about.html', title='about')
 
 if __name__== '__main_':
     app.run(debug=True)
