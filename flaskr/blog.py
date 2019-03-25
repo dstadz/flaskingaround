df27a6ef4542abe61dff1ce8dbdc503b847dbcad
Drazen <danstad2012@gmail.com>
2019-03-25 15:23:45 -0700

    registration err

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'd2c736362984d2591ea5d5669693b752'
 
 posts = [
     {
@@ -27,6 +30,19 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='about')
+
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form='form')
+    
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form='form')
+
 
 if __name__== '__main_':
     app.run(debug=True)
