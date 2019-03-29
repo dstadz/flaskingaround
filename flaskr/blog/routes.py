65becb61d9bf6a8681aa35e14158f3d8d04e1e9f
Drazen <danstad2012@gmail.com>
2019-03-29 15:06:02 -0700

    fix form bug

--- flaskr/blog/routes.py
+++ flaskr/blog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from blog import app, db, bcrypt
-from blog.forms import RegistrationForm, LoginForm
+from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -71,6 +71,7 @@ def logout():
 @app.route('/account')
 @login_required
 def account():
-    logout_user()
-    return render_template('account.html', title='account')
+    form =  UpdateAccountForm()
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='account', image_file=image_file)
     
