3e0d87af705e9289dcc46b426576cb71ab43933e
Drazen <danstad2012@gmail.com>
2019-04-03 19:58:40 -0700

    tiny bug fix

--- flaskr/blog/routes.py
+++ flaskr/blog/routes.py
@@ -141,7 +141,7 @@ def delete_post(post_id):
 
 
 @app.route("/user/<string:username>")
-def user_posts():
+def user_posts(username):
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
     posts = Post.query.filter_by(author=user)\
